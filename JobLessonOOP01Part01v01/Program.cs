341fb76204f4352855548537b7602465e326c863
Safroniv <safroniv@mail.ru>
2022-03-28 00:10:40 +0300

    Добавьте файлы проекта.

---
+++ JobLessonOOP01Part01v01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JobLessonOOP01Part01v01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
